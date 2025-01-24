85fd311b58a8cc4edf6f8af438482e38ea4d510c
luna :3 <ellipticobj@gmail.com>
2025-01-24 22:23:04 +0800

    err some things are broken right now

--- app.py
+++ app.py
@@ -6,47 +6,19 @@ import os
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+projects = [
+    {"name": "CLI Messenger App", "description": "A simple messenger app in the command line made in Python.", "github": "http://github.com/ellipticobj/cli-messenger"},
+    {"name": "Material You Pomodoro Timer", "description": "A pomodoro study timer app made using Jetpack Compose in Kotlin.", "github": "http://github.com/ellipticobj/studytimer"},
+    {"name": "Random PIP Module", "description": "A small pip module that generates true random numbers using Random.org's API.", "github": "http://github.com/ellipticobj/random-module", "pypi": "https://pypi.org/project/randomorg-api/"},
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-'''
-@app.route('/projects/example')
-def projectshell(projname):
-    projpath = os.path.join('projects', f"{projname}.py")
-
-    if os.path.exists(projpath):
-        return render_template('shell.html', projname=projname)
-    else:
-        return f"project {projname} not found", 404
-'''
+    return render_template('index.html', projects=projects)
 
 @app.route('/projects')
 def projectspage():
-    return render_template('projects.html')
-
-
-@socketio.on("execute")
-def execute(data):
-    projname = data.get('projname')
-    command = data.get('command')
-    
-    projpath = os.path.join('projects', f"{projname}.py")
-    
-    if os.path.exists(projpath):
-        process = subprocess.Popen(
-            ['python3', projpath],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        
-        stdout, stderr = process.communicate(input=command)
-        emit('output', {'output': stdout or stderr})
-    
-    else:
-        emit('output', {'output': f"error: project {projname} not found"})
-        
+    return render_template('projects.html', projects=projects)
+       
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=7272, debug=True)
