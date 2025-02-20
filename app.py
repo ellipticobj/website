4e0047030757467d34ff891b283ecb98f79c9ef0
luna <luna@hackclub.app>
2025-02-20 17:35:06 +0000

    updates to app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import *
-from flask_cors import *
+from flask import Flask, render_template
+from flask_cors import CORS, jsonify
 
 app = Flask(__name__)
 CORS(app)
@@ -96,6 +96,7 @@ def projectspage():
 @app.route('/projects/json/')
 def projectsapi():
     projectdir = os.path.abspath('projects')
+    print(projectdir)
     return jsonify(projects)
 
 @app.route('/shells')
